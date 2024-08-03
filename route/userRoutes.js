66c5c9e08e24166190be92a26e958a0bc6acd9f1
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2024-08-03 07:24:14 +0530

    Update userRoutes.js

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,5 +1,11 @@
-const {register, login} = require("../controllers/usercontroller");
+const { register, login } = require("../controllers/usercontroller");
 const router = require("express").Router();
-router.post("/register",register);
-router.post("/login",login);
+
+router.post("/register", register);
+router.post("/login", login);
+
+router.get("*", (req, res) => {
+    res.redirect("/healthcare/login");
+});
+
 module.exports = router;
